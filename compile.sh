022dc5f4b2cdd32b52775ce8feda82012fafc4a6
Max <maksbot22@mail.ru>
2022-05-19 21:10:32 +0300

    clean repo

--- compile.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-clang -g -O0 -Wall -Wextra -Iminilib *.c minilib/*.c -lm -o ping
-sudo chown root:root ping
-sudo chmod u+s ping
--- compileo2.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-clang -O2 -Wall -Wextra -Iminilib *.c minilib/*.c -lm -o ping
-sudo chown root:root ping
-sudo chmod u+s ping
